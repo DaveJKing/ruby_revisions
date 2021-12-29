c0dd66abfef9e3d328243b5c41988624173b5262
Dave J King <{ID}+{username}@users.noreply.github.com>
2021-12-29 18:33:33 +0000

    good work

--- arraysandobjects.rb
+++ arraysandobjects.rb
@@ -1,10 +1,12 @@
 puts "running Checkout"
 
 class LinearDiscount
+  # puts "hello from initialise Linear Discount"
+
+  attr_reader :discount_price ,:quantity
   def initialize(discount_price, quantity)
     @discount_price = discount_price
     @quantity = quantity
-    puts "hello from initialise Linear Discount"
   end
 
   #discount price type is linear
@@ -15,10 +17,12 @@ class LinearDiscount
 end
 
 class BatchDiscount
+  attr_reader :batch_threshold_trigger, :gets_free, :occurs
   def initialize(batch_threshold_trigger, gets_free, occurs)
-    puts "hello from initialise Batch Discount"
+    
+    # puts "hello from initialise Batch Discount"
 
-    @batch_threshold_trigger = batch_threshold_trigger,
+    @batch_threshold_trigger = batch_threshold_trigger
     @gets_free = gets_free
     @occurs = occurs
   end
@@ -31,6 +35,8 @@ class BatchDiscount
 end
 
 class PricePolicy
+  attr_reader :base_price, :discounts
+
   def initialize(base_price, *discounts)
     @base_price = base_price
     @discounts = discounts
@@ -54,9 +60,9 @@ RULES = {
   "C" => PricePolicy.new(20),
   "D" => PricePolicy.new(15),
   "E" => PricePolicy.new(30, BatchDiscount.new(2, 1, 1)),
-    # Above buy 2 get 1 free , only one cycle
-  "F" => PricePolicy.new(30, BatchDiscount.new(2, 1, -1)),
-  # Above buy 2 get 1 free , perpetual  i.e no limit
+  # Above buy 2 get 1 free , only one cycle
+  "F" => PricePolicy.new(30, BatchDiscount.new(3, 2, -1)),
+# Above buy 2 get 1 free , perpetual  i.e no limit
 
 }
 puts RULES
@@ -64,5 +70,36 @@ puts RULES.class
 
 puts RULES.has_key?("A")
 puts RULES.has_key?("X")
+
+#user.extract!("email").values # ["john@email.com"]
+
+# for number in RULES
+#   print number
+# end
+puts RULES["F"]
+
+x = RULES["F"]
+y = x
+puts "item base price : " + x.base_price.to_s
+puts x.discounts[0].class.name
+puts x.class.name
+
+case x.discounts[0].class.name
+when "LinearDiscount", "lineardiscount"
+  puts "Linear Discount encountered"
+  puts "Item is sold at " + x.base_price.to_s + " initially. then " + x.discounts[0].discount_price.to_s + " after quantity of " + x.discounts[0].quantity.to_s
+when "BatchDiscount", "baychdiscount"
+  puts "Batch Discount encountered"
+  puts "Item is sold at " + x.base_price.to_s + " initially, then after " + x.discounts[0].batch_threshold_trigger.to_s + " item purchases buyer gets " + x.discounts[0].gets_free.to_s
+  if x.discounts[0].occurs == -1
+    puts "this discount cycles indefinitely !"
+  else
+    puts "this discount cycles " + x.discounts[0].occurs.to_s + " times(s) only"
+  end
+else
+  puts "unknown discount policy"
+end
+
+# puts RULES["A"]
 
 puts "end"
