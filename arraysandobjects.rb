21080577111d34f2daeb26447e9bfe129b030a0d
Dave J King <{ID}+{username}@users.noreply.github.com>
2022-01-05 12:53:45 +0000

    linear total added to class

--- arraysandobjects.rb
+++ arraysandobjects.rb
@@ -7,6 +7,7 @@ puts (debug ? "Debug mode operating " : "Not in debug mode")
 
 # Set rules for priceplocy and discounts
 # Item pricing policy would normally be consumed from a db.
+# This is has global scope to facilitate that simulation
 
 RULES = {
   "A" => PricePolicy.new(50, LinearDiscount.new(30, 3)),
@@ -16,83 +17,23 @@ RULES = {
   "E" => PricePolicy.new(30, BatchDiscount.new(2, 1, 1)),
   # Above buy 2 get 1 free , only one cycle
   "F" => PricePolicy.new(30, BatchDiscount.new(3, 2, -1)),
-# Above buy 2 get 1 free , perpetual  i.e no limit
+  # Above buy 2 get 1 free , perpetual  i.e no limit
 }
 
-class Checkout
-
-  attr_accessor  :loyaltycardno, :items
-
-  def initialize()
-    @items = Hash.new
-  end
-
-  def scan(item,*loyaltyCardNoRef)
-
-    if item.downcase != "clubcard"
-      @items[item] ||= 0
-      @items[item] += 1
-    end
-   
-    print "Scanned item #{item},  " 
-    self.loyaltycardno =loyaltyCardNoRef[0].to_s unless loyaltyCardNoRef.empty?
-  end
-  
-end
-
-co = Checkout.new
-co.scan("A")
-co.scan("A")
-co.scan("A")
-co.scan("A")
-co.scan("A")
-# co.scan("B")
- co.scan("Clubcard","123456")
-# co.scan("A")
-# co.scan("C")
-# co.scan("F")
-# co.scan("Clubcard","123456")
-# co.scan("Voucher","999")
-
-puts (co.loyaltycardno.nil? ? "No loyalty card scanned " : "Loyalty card #{co.loyaltycardno} was scanned " )
-
-# puts RULES
-# puts (RULES.has_key?("A") ? "Key exists :-) " : "Key does not exist :-(")
-
-itemHash = co.items
-for item,itemQuanityPurchased in itemHash
-  puts "#{item},#{itemQuanityPurchased}"
-  showDiscountStrategy(RULES[item],debug)
-
-  itemRule = RULES[item]
-  itemRuleDiscount = itemRule.discounts[0]
-
-  puts itemRule.base_price
-  puts "Discount price is #{itemRuleDiscount.discount_price}, kicks in after purchase quantity #{itemRuleDiscount.quantity_threshold } "
 
 
-  # linear
-  if itemQuanityPurchased > itemRuleDiscount.quantity_threshold 
-    puts "Threshold passed !"
-    baseTotal = itemRule.base_price * (itemRuleDiscount.quantity_threshold )
+checkout = Checkout.new
+checkout.scan("A")
+checkout.scan("A")
+checkout.scan("A")
+checkout.scan("A")
+# checkout.scan("X")
 
-    discountQty = itemQuanityPurchased - (itemRuleDiscount.quantity_threshold )
-    discountTotal = discountQty * itemRuleDiscount.discount_price
-    total = baseTotal + discountTotal
-  
-  else
-    puts "NOT Threshold reached !"
-    total = itemQuanityPurchased * itemRule.base_price
-  end
+checkout.scan("Clubcard","123456")
 
-  puts "Total : #{total}"
+puts (checkout.loyaltycardno.nil? ? "No loyalty card scanned " : "Loyalty card #{checkout.loyaltycardno} was scanned " )
 
-end
-
-
-
-# itemRule = RULES["A"]
-# showDiscountStrategy(itemRule,debug)
-
+checkout.total_up
+puts "Basket total : #{checkout.basket_total.to_s} pence"
 
 puts "End of checkout !"
--- lib/checkoutObjects.rb
+++ lib/checkoutObjects.rb
@@ -1,44 +1,83 @@
-
 puts "Loading objects ....."
-class PricePolicy
-    attr_reader :base_price, :discounts
-  
-    def initialize(base_price, *discounts)
-      @base_price = base_price
-      @discounts = discounts
+
+class Checkout
+    attr_accessor  :loyaltycardno, :items, :basket_total
+    def initialize()
+      @items = Hash.new
+      @basket_total = 0
     end
   
-    # def price_for(quantity)
-    #   quantity * @base_price - discount_for(quantity)
-    # end
-  
-    # def discount_for(quantity)
-    #   @discounts.inject(0) do |mem, discount|
-    #     mem + discount.calculate_for(quantity)
-    #   end
-    # end
+    def scan(item,*loyaltyCardNoRef)
+      if item.downcase != "clubcard"
+        begin
+            check = RULES.fetch(item)
+        rescue => exception
+            # item is not in stock database, handle and report 
+            puts "Inform shopper Item removed fom sale to be polite."
+        else
+            @items[item] ||= 0
+            @items[item] += 1
+        end
+      end 
+      self.loyaltycardno =loyaltyCardNoRef[0].to_s unless loyaltyCardNoRef.empty?
+    end
+    
+
+    def total_up
+    
+        for item,itemQuanityPurchased in @items
+        
+            itemRule = RULES[item]
+            itemRuleDiscount = itemRule.discounts[0]
+
+            case itemRule.discounts[0].class.name
+            when "LinearDiscount", "lineardiscount"
+                if itemQuanityPurchased > itemRuleDiscount.quantity_threshold 
+                    # "Discount threshold passed !"
+                  
+                  baseTotal = itemRule.base_price * (itemRuleDiscount.quantity_threshold )
+            
+                  discountQty = itemQuanityPurchased - (itemRuleDiscount.quantity_threshold )
+                  discountTotal = discountQty * itemRuleDiscount.discount_price
+                  @basket_total = baseTotal + discountTotal
+                
+                else
+                    # "Discount hreshold NOT reached !"
+                    @total_total = itemQuanityPurchased * itemRule.base_price
+                end
+            end
+         end
+    end
+
+end
+
+class PricePolicy
+  attr_reader :base_price, :discounts
+
+  def initialize(base_price, *discounts)
+    @base_price = base_price
+    @discounts = discounts
   end
 
+end
 
 class LinearDiscount
-    # puts "hello from initialise Linear Discount"
-  
-    attr_reader :discount_price ,:quantity_threshold
-    def initialize(discount_price, quantity_threshold)
-      @discount_price = discount_price
-      @quantity_threshold = quantity_threshold
-    end
-  
+  attr_reader :discount_price, :quantity_threshold
+
+  def initialize(discount_price, quantity_threshold)
+    @discount_price = discount_price
+    @quantity_threshold = quantity_threshold
   end
+end
 
 class BatchDiscount
-attr_reader :batch_threshold_trigger, :gets_free, :occurs
-def initialize(batch_threshold_trigger, gets_free, occurs)
-    
-    # puts "hello from initialise Batch Discount"
+  attr_reader :batch_threshold_trigger, :gets_free, :occurs
 
+  def initialize(batch_threshold_trigger, gets_free, occurs)
     @batch_threshold_trigger = batch_threshold_trigger
     @gets_free = gets_free
     @occurs = occurs
-end
+  end
+
+  
 end
